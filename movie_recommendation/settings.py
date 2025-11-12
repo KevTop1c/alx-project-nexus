e44e03b78ea18d92d0b825d45cec1612eb4c0b4d
Kevin <kev@django.com>
2025-11-12 21:11:23 +0000

    build: add logging configuration

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
