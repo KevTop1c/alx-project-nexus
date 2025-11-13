bdcd09e2395192736fdff38413e5f02b1502d6b3
Kevin <kev@django.com>
2025-11-13 10:10:08 +0000

    feat: update login endpoint with enhanced JWT response

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, login_view, profile_view
+from .views import RegisterView, LoginView, profile_view
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", login_view, name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("profile/", profile_view, name="profile"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
