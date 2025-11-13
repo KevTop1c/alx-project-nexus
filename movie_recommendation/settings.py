c20713da2b1f9d9b874da64c59a43c5d3850889e
Kevin <kev@django.com>
2025-11-13 09:57:08 +0000

    build: update REST_FRAMEWORK configuration to include SessionAuthentication and renderers

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -99,8 +99,13 @@ TMDB_BASE_URL = config("TMDB_BASE_URL")
 
 # REST Framework configuration
 REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer", 
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
