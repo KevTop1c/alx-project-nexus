b9b25d06d9b17b4221d89b8394eb639f85a3cef9
Kevin <kev@django.com>
2025-11-20 18:15:09 +0000

    refactor: revert to original codebase

--- users/apps.py
+++ users/apps.py
@@ -1,28 +1,9 @@
 import logging
 from django.apps import AppConfig
 
+# logger = logging.getLogger(__name__)
+
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
-
-    def ready(self):
-        # Import signals only after app registry is ready
-        try:
-            import users.signals
-        except ImportError as e:
-            # Log error but don't crash the app
-            logging.getLogger(__name__).warning("Failed to import users signals %s", e)
-
-        # Backfill profiles for users who have no profile
-        try:
-            from django.contrib.auth import get_user_model
-            from users.models import Profile
-
-            User = get_user_model()
-
-            for user in User.objects.all():
-                Profile.objects.get_or_create(user=user)
-
-        except Exception as e:
-            logging.getLogger(__name__).warning("Failed to backfill profiles: %s", e)
